9f574a5b65eed3a15dd7da74b3fe3fe15b3e75b7
Harishankar-S <100881953+Harishankar-S@users.noreply.github.com>
2022-05-04 20:16:16 -0500

    Update Photo.java

--- Photo.java
+++ Photo.java
@@ -1,35 +1,13 @@
-import java.time.Year;
-
-public final class Photo extends Media{
-    
-    public Photo(String title, int year) {
-        super(title, year);
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-    
-    public int getYear() {
-        return year;
-    }
-
-    public void setTitle(String title) throws IllegalArgumentException
-    {
-        if(title.isBlank()) {throw new IllegalArgumentException("Title cannot be blank.");}
-    }
-    
-    
-    public void setYear(int year) throws IllegalArgumentException
-    {
-        if(year < 1878) {throw new IllegalArgumentException("Year cannot be before 1878.");}
-        if(year > Year.now().getValue() + 1) {throw new IllegalArgumentException("Year cannot be in the future.");}
-    }
-    
-    @Override
-    public String toString() {
-        
-        return String.format("%s, [ %d ]",title, year);
-    
-}
+public final class Photo extends Media
+{
+	public Photo(String title, int year)
+	{
+		super(title, year);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format("%s, [ %d ]",title, year);
+	}
 }
