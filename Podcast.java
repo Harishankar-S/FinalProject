d9085cc61438166e59a28adb941a41833ec421fe
Alexhb1233 <100882144+Alexhb1233@users.noreply.github.com>
2022-05-05 22:15:07 -0500

    Update Podcast.java

--- Podcast.java
+++ Podcast.java
@@ -1,46 +1,53 @@
 public class Podcast extends Media implements Audible
 {
-	private String creator;
-	private int season;
-	private int episode;
-	
-	public Podcast(String title, String creator, int year, int season, int episode)
-	{
-		super(title, year);
-		setCreator(creator);
-		setEpisode(episode);
-		setSeason(season);
-	}
+    private String creator;
+    private int season;
+    private int episode;
+    private int rating;
+    
+    public Podcast(String title, String creator, int year, int season, int episode, int rating)
+    {
+        super(title, year);
+        setCreator(creator);
+        setEpisode(episode);
+        setSeason(season);
+        setRating(rating);
+    }
 
-	public String getCreator() {return this.creator;}
-	public int getSeason() {return this.season;}
-	public int getEpisode() {return this.episode;}
+    public String getCreator() {return this.creator;}
+    public int getSeason() {return this.season;}
+    public int getEpisode() {return this.episode;}
+    public int getRating() {return this.rating;}
+    
+    public void setCreator(String creator) throws IllegalArgumentException
+    {
+        if(creator.isBlank()) {throw new IllegalArgumentException("Creator cannot be blank.");}
+        this.creator = creator;
+    }
+    
+    public void setSeason(int season) throws IllegalArgumentException
+    {
+        if(season <= 0) {throw new IllegalArgumentException("Season cannot be 0 or less.");}
+        this.season = season;
+    }
 
-	
-	public void setCreator(String creator) throws IllegalArgumentException
-	{
-		if(creator.isBlank()) {throw new IllegalArgumentException("Creator cannot be blank.");}
-		this.creator = creator;
-	}
-	
-	public void setSeason(int season) throws IllegalArgumentException
-	{
-		if(season <= 0) {throw new IllegalArgumentException("Season cannot be 0 or less.");}
-		this.season = season;
-	}
 
-
-	public void setEpisode(int episode) throws IllegalArgumentException
-	{
-		if(episode <= 0) {throw new IllegalArgumentException("Episode cannot be 0 or less.");}
-		this.episode = episode;
-	}
-	
-	
-	@Override
-	public String toString()
-	{
-		return String.format("Podcast: [Creator: %s, Title: %s, Season: %d, Episode: %d, Year: %d]",
-				this.creator, this.title, this.season, this.episode, this.year);
-	}
+    public void setEpisode(int episode) throws IllegalArgumentException
+    {
+        if(episode <= 0) {throw new IllegalArgumentException("Episode cannot be 0 or less.");}
+        this.episode = episode;
+    }
+    
+    public void setRating(int rating) 
+    {
+        this.rating = rating;
+    }
+    
+    
+    @Override
+    public String toString()
+    {
+        return String.format("Podcast: [Creator: %s, Title: %s, Season: %d, Episode: %d, Year: %d, Rating: %d",
+                this.creator, this.title, this.season, this.episode, this.year, this.rating);
+    }
 }
