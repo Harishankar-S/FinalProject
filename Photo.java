ddef19497a9cc108d3629bdedd16b4d37fd642d0
Alexhb1233 <100882144+Alexhb1233@users.noreply.github.com>
2022-05-05 22:14:39 -0500

    Update Photo.java

--- Photo.java
+++ Photo.java
@@ -1,14 +1,26 @@
 public final class Photo extends Media implements Viewable
 {
-	public Photo(String title, int year)
-	{
-		super(title, year);
-	}
-	
-	@Override
-	public String toString()
-	{
-		return String.format("Photo: [Title: %s, Year: %d]",
-				this.title, this.year);
-	}
+    
+   private int rating;
+    
+    public Photo(String title, int year, int rating)
+    {
+        super(title, year);
+        setRating(rating);
+    }
+    
+ public int getRating() {return this.rating;}
+ 
+  public void setRating(int rating) 
+    {
+        this.rating = rating;
+
+    }
+    
+    @Override
+    public String toString()
+    {
+        return String.format("Photo: [Title: %s, Year: %d, Rating: %s]",
+                this.title, this.year, this.rating);
+    }
 }
