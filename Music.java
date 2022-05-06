7fedd75ed48b14f769d84769fca87449250af27d
Alexhb1233 <100882144+Alexhb1233@users.noreply.github.com>
2022-05-05 22:11:21 -0500

    Update Music.java

--- Music.java
+++ Music.java
@@ -1,37 +1,44 @@
 public class Music extends Media implements Audible
 {
-	private String artist;
-	private String genre;
-	
-	public Music(String title, String artist, int year, String genre)
-	{
-		super(title, year);
-		setArtist(artist);
-		setYear(year);
-		setGenre(genre);
-	}
+    private String artist;
+    private String genre;
+    private int rating;
+    
+    public Music(String title, String artist, int year, String genre, int rating)
+    {
+        super(title, year);
+        setArtist(artist);
+        setYear(year);
+        setGenre(genre);
+        setRating(rating);
+    }
 
-	
-	public String getArtist() {return this.artist;}
-	public String getGenre() {return this.genre;}
+    
+    public String getArtist() {return this.artist;}
+    public String getGenre() {return this.genre;}
+    public int getRating() {return this.rating;}
 
+    public void setArtist(String artist) throws IllegalArgumentException
+    {
+        if(artist.isBlank()) {throw new IllegalArgumentException("Artist cannot be blank.");}
+        this.artist = artist;
+    }
 
-	public void setArtist(String artist) throws IllegalArgumentException
-	{
-		if(artist.isBlank()) {throw new IllegalArgumentException("Artist cannot be blank.");}
-		this.artist = artist;
-	}
+    public void setGenre(String genre) throws IllegalArgumentException
+    {
+        if(genre.isBlank()) {throw new IllegalArgumentException("Genre cannot be blank.");}
+        this.genre = genre;
+    }
+    
+    public void setRating(int rating) 
+    {
+        this.rating = rating;
+    }
 
-	public void setGenre(String genre) throws IllegalArgumentException
-	{
-		if(genre.isBlank()) {throw new IllegalArgumentException("Genre cannot be blank.");}
-		this.genre = genre;
-	}
-
-	@Override
-	public String toString()
-	{
-		return String.format("Music: [Title: %s, Artist: %s, Year: %d, Genre: %s]",
-				this.title, this.artist, this.year, this.genre);
-	}
+    @Override
+    public String toString()
+    {
+        return String.format("Music: [Title: %s, Artist: %s, Year: %d, Genre: %s, Rating: %d]",
+                this.title, this.artist, this.year, this.genre, this.rating);
+    }
 }
