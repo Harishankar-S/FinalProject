fe86cafd456d7f72a50fd9a99c073a72fc1153a1
Alexhb1233 <100882144+Alexhb1233@users.noreply.github.com>
2022-05-05 22:11:00 -0500

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,37 +1,44 @@
 public class Movie extends Media implements Audible, Viewable
 {
-	private String director;
-	private String genre;
-	
-	public Movie(String title, String director, int year, String genre)
-	{
-		super(title, year);
-		setDirector(director);
-		setYear(year);
-		setGenre(genre);
-	}
+    private String director;
+    private String genre;
+    private int rating;
+    
+    public Movie(String title, String director, int year, String genre, int rating)
+    {
+        super(title, year);
+        setDirector(director);
+        setYear(year);
+        setGenre(genre);
+        setRating(rating);
+    }
 
-	
-	public String getDirector() {return this.director;}
-	public String getGenre() {return this.genre;}
+    
+    public String getDirector() {return this.director;}
+    public String getGenre() {return this.genre;}
+    public int getRating() {return this.rating;}
 
+    public void setDirector(String director) throws IllegalArgumentException
+    {
+        if(director.isBlank()) {throw new IllegalArgumentException("Director cannot be blank.");}
+        this.director = director;
+    }
 
-	public void setDirector(String director) throws IllegalArgumentException
-	{
-		if(director.isBlank()) {throw new IllegalArgumentException("Director cannot be blank.");}
-		this.director = director;
-	}
+    public void setGenre(String genre) throws IllegalArgumentException
+    {
+        if(genre.isBlank()) {throw new IllegalArgumentException("Genre cannot be blank.");}
+        this.genre = genre;
+    }
+    
+    public void setRating(in rating) throws IllegalArgumentException
+    {
+        this.rating = rating;
+    }
 
-	public void setGenre(String genre) throws IllegalArgumentException
-	{
-		if(genre.isBlank()) {throw new IllegalArgumentException("Genre cannot be blank.");}
-		this.genre = genre;
-	}
-
-	@Override
-	public String toString()
-	{
-		return String.format("Movie: [Title: %s, Director: %s, Year: %d, Genre: %s]",
-				this.title, this.director, this.year, this.genre);
-	}
+    @Override
+    public String toString()
+    {
+        return String.format("Movie: [Title: %s, Director: %s, Year: %d, Genre: %s, Rating: %d]",
+                this.title, this.director, this.year, this.genre, this.rating);
+    }
 }
